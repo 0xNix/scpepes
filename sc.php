17e84a1c44cec8cf42255e598bc626a4a6e56c56
DenisHys <43570272+0xNix@users.noreply.github.com>
2022-02-15 13:42:22 -0800

    Add files via upload

---
+++ sc.php
@@ -0,0 +1,43 @@
+
+<html>
+ 
+<head>
+    <title>Hacked by 0xNix </title>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+    <meta property="og:title" content="Hacked by 0xNix">
+    <meta name="description" content="wh00pz ! , your security get down">
+    <meta property="og:description" content="wh00pz ! , your security get down">
+    <meta name="theme-color" content="#fff">
+    <style type='text/css'>body,a:hover{cursor:url(../cur.cursors-4u.net/symbols/sym-1/sym46.cur),progress!important;}</style>
+</head>
+<body bgcolor="white" text="black" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;">
+<style type="text/css">
+    center {
+        font-family: Courier;
+    }
+    img {
+        opacity: 80%;
+    }
+    red {
+        color: red;
+    }
+</style>
+<table width="100%" height="80%">
+    <td>
+        <center>
+
+            <img src="https://i.postimg.cc/3xgBgHjp/0xNix.png" width="210" height="155">
+            <font size="2"><br>security is just an illusion<br> </font><br>
+            <font size="2"><i>Greetz:</i><br>
+           0xShall - DENNI5.ID - Zero9Fadera - Culutax07 - ./FakeRoot - Mr'Pl4NKT0N - Caisar ID
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+           
+            </font>
+    </td>
+</table>
